b6e8e14c64a1d302eec03af76a8d7d3d53b13107
Vitor Martins <vitor.martins@fever.pt>
2020-06-22 16:56:51 +0100

    Authentication Project VUE

---
+++ src/axios-auth.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
+})
+
+
+// instance.defaults.headers.common['SOMETHING'] = 'something'
+
+export default instance
