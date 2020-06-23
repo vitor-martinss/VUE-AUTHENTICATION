cd6fea18abd46d1020c2bc3f0fcfa84504e1ae26
Vitor Martins <vitor.martins@fever.pt>
2020-06-23 01:02:18 +0100

    Finished authentication

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -1,141 +1,185 @@
 <template>
-<div id="signup">
-    <div class="signup-form">
-        <form @submit.prevent="onSubmit">
-            <div class="input" :class="{invalid: $v.email.$error}">
-                <label for="email">Mail</label>
-                <input type="email" id="email" @blur='$v.email.$touch()' v-model="email" >
-				<p v-if="!$v.email.email">Please provide a valid email address.</p>
-				<p v-if="!$v.email.required">This field must not be empty</p>
-            </div>
-            <div class="input">
-                <label for="age">Your Age</label>
-                <input type="number" id="age" v-model.number="age">
-            </div>
-            <div class="input">
-                <label for="password">Password</label>
-                <input type="password" id="password" v-model="password">
-            </div>
-            <div class="input">
-                <label for="confirm-password">Confirm Password</label>
-                <input type="password" id="confirm-password" v-model="confirmPassword">
-            </div>
-            <div class="input">
-                <label for="country">Country</label>
-                <select id="country" v-model="country">
-                    <option value="usa">USA</option>
-                    <option value="india">India</option>
-                    <option value="uk">UK</option>
-                    <option value="germany">Germany</option>
-                </select>
-            </div>
-            <div class="hobbies">
-                <h3>Add some Hobbies</h3>
-                <button @click="onAddHobby" type="button">Add Hobby</button>
-                <div class="hobby-list">
-                    <div class="input" v-for="(hobbyInput, index) in hobbyInputs" :key="hobbyInput.id">
-                        <label :for="hobbyInput.id">Hobby #{{ index }}</label>
-                        <input type="text" :id="hobbyInput.id" v-model="hobbyInput.value">
-                        <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
-                    </div>
-                </div>
-            </div>
-            <div class="input inline">
-                <input type="checkbox" id="terms" v-model="terms">
-                <label for="terms">Accept Terms of Use</label>
-            </div>
-            <div class="submit">
-                <button type="submit">Submit</button>
-            </div>
-        </form>
-    </div>
-</div>
+	<div id="signup">
+		<div class="signup-form">
+			<form @submit.prevent="onSubmit">
+				<div class="input" :class="{invalid: $v.email.$error}">
+					<label for="email">Mail</label>
+					<input type="email" id="email" @blur='$v.email.$touch()' v-model="email">
+					<p v-if="!$v.email.email">Please provide a valid email address.</p>
+					<p v-if="!$v.email.required">This field must not be empty</p>
+					<p v-if="!$v.email.unique">This email is already registered</p>
+				</div>
+				<div class="input" :class="{invalid: $v.age.$error}">
+					<label for="age">Your Age</label>
+					<input type="number" id="age" @blur='$v.age.$touch()' v-model.number="age">
+					<p v-if="!$v.age.minValue">You have to be at least {{ $v.age.$params.minValue.min }} years old</p>
+				</div>
+				<div class="input" :class="{invalid: $v.password.$error}">
+					<label for="password">Password</label>
+					<input type="password" id="password" @blur='$v.password.$touch()' v-model="password">
+					<p v-if="!$v.password.minLength">Your password must have at least {{ $v.password.$params.minLength.min }} characters</p>
+				</div>
+				<div class="input" :class="{invalid: $v.confirmPassword.$error}">
+					<label for="confirm-password">Confirm Password</label>
+					<input type="password" id="confirm-password" @blur='$v.confirmPassword.$touch()' v-model="confirmPassword">
+					<p v-if="!$v.confirmPassword.sameAs">It has to be the same of your password</p>
+				</div>
+				<div class="input">
+					<label for="country">Country</label>
+					<select id="country" v-model="country">
+			                    <option value="usa">USA</option>
+			                    <option value="india">India</option>
+			                    <option value="uk">UK</option>
+			                    <option value="germany">Germany</option>
+			                </select>
+				</div>
+				<div class="hobbies">
+					<h3>Add some Hobbies</h3>
+					<button @click="onAddHobby" type="button">Add Hobby</button>
+					<div class="hobby-list">
+						<div class="input" v-for="(hobbyInput, index) in hobbyInputs" :key="hobbyInput.id" :class="{invalid: $v.hobbyInputs.$each[index].$error}">
+							<label :for="hobbyInput.id">Hobby #{{ index }}</label>
+							<input type="text" :id="hobbyInput.id" @blur='$v.hobbyInputs.$each[index].value.$touch()' v-model="hobbyInput.value">
+							<button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
+						</div>
+						<p v-if="!$v.hobbyInputs.minLen">You have to specify at least {{ $v.hobbyInputs.$params.minLen.min }} hobbies</p>
+						<p v-if="!$v.hobbyInputs.required">Please add hobbies</p>
+					</div>
+				</div>
+				<div class="input inline" :class="{invalid: $v.terms.$invalid}">
+					<input type="checkbox" id="terms" @change='$v.terms.$touch()' v-model="terms">
+					<label for="terms">Accept Terms of Use</label>
+				</div>
+				<div class="submit">
+					<button type="submit" :disabled='$v.$invalid'>Submit</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
 
 <script>
-import { required, email } from 'vuelidate/lib/validators'
+import { required, email, numeric, minValue, minLength, sameAs, requiredUnless } from 'vuelidate/lib/validators'
+import axios from 'axios'
 export default {
-    data() {
-        return {
-            email: '',
-            age: null,
-            password: '',
-            confirmPassword: '',
-            country: 'usa',
-            hobbyInputs: [],
-            terms: false
-        }
+	data() {
+		return {
+			email: '',
+			age: null,
+			password: '',
+			confirmPassword: '',
+			country: 'usa',
+			hobbyInputs: [],
+			terms: false
+		}
 	},
 	validations: {
 		email: {
 			required: required,
-			email: email
+			email: email,
+			unique: val => {
+				if (val === '') return true
+				return axios.get('/users.json?orderBy="email"&equalTo="' + val + '"')
+					.then(res => {
+						return Object.keys(res.data).length === 0
+					})
+			}
+		},
+		age: {
+			required: required,
+			numeric,
+			minValue: minValue(18)
+		},
+		password: {
+			required: required,
+			minLength: minLength(6)
+		},
+		confirmPassword: {
+			sameAs: sameAs('password')
+		},
+		terms: {
+			required,
+			chacked(val) {
+				//console.log(val);
+				return this.country === 'germany' ? true : val
+			},
+			required2: requiredUnless(vm => {
+				//console.log(vm);
+				return vm.country === 'Germany'
+			})
+		},
+		hobbyInputs: {
+			required,
+			minLen: minLength(2),
+			$each: {
+				value: {
+					required: minLength(5)
+				}
+			}
 		}
 	},
-    methods: {
-        onAddHobby() {
-            const newHobby = {
-                id: Math.random() * Math.random() * 1000,
-                value: ''
-            }
-            this.hobbyInputs.push(newHobby)
-        },
-        onDeleteHobby(id) {
-            this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id)
-        },
-        onSubmit() {
-            const formData = {
-                email: this.email,
-                age: this.age,
-                password: this.password,
-                confirmPassword: this.confirmPassword,
-                country: this.country,
-                hobbies: this.hobbyInputs.map(hobby => hobby.value),
-                terms: this.terms
-            }
-            console.log(formData)
-            this.$store.dispatch('signup', formData)
-        }
-    }
+	methods: {
+		onAddHobby() {
+			const newHobby = {
+				id: Math.random() * Math.random() * 1000,
+				value: ''
+			}
+			this.hobbyInputs.push(newHobby)
+		},
+		onDeleteHobby(id) {
+			this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id)
+		},
+		onSubmit() {
+			const formData = {
+				email: this.email,
+				age: this.age,
+				password: this.password,
+				confirmPassword: this.confirmPassword,
+				country: this.country,
+				hobbies: this.hobbyInputs.map(hobby => hobby.value),
+				terms: this.terms
+			}
+			console.log(formData)
+			this.$store.dispatch('signup', formData)
+		}
+	}
 }
 </script>
 
 <style scoped>
 .signup-form {
-    width: 400px;
-    margin: 30px auto;
-    border: 1px solid #eee;
-    padding: 20px;
-    box-shadow: 0 2px 3px #ccc;
+	width: 400px;
+	margin: 30px auto;
+	border: 1px solid #eee;
+	padding: 20px;
+	box-shadow: 0 2px 3px #ccc;
 }
 
 .input {
-    margin: 10px auto;
+	margin: 10px auto;
 }
 
 .input label {
-    display: block;
-    color: #4e4e4e;
-    margin-bottom: 6px;
+	display: block;
+	color: #4e4e4e;
+	margin-bottom: 6px;
 }
 
 .input.inline label {
-    display: inline;
+	display: inline;
 }
 
 .input input {
-    font: inherit;
-    width: 100%;
-    padding: 6px 12px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
+	font: inherit;
+	width: 100%;
+	padding: 6px 12px;
+	box-sizing: border-box;
+	border: 1px solid #ccc;
 }
-
 
 .input.invalid label {
 	color: red;
 }
-
 
 .input.invalid input {
 	border: .1rem solid red;
@@ -143,58 +187,58 @@ export default {
 }
 
 .input.inline input {
-    width: auto;
+	width: auto;
 }
 
 .input input:focus {
-    outline: none;
-    border: 1px solid #521751;
-    background-color: #eee;
+	outline: none;
+	border: 1px solid #521751;
+	background-color: #eee;
 }
 
 .input select {
-    border: 1px solid #ccc;
-    font: inherit;
+	border: 1px solid #ccc;
+	font: inherit;
 }
 
 .hobbies button {
-    border: 1px solid #521751;
-    background: #521751;
-    color: white;
-    padding: 6px;
-    font: inherit;
-    cursor: pointer;
+	border: 1px solid #521751;
+	background: #521751;
+	color: white;
+	padding: 6px;
+	font: inherit;
+	cursor: pointer;
 }
 
 .hobbies button:hover,
 .hobbies button:active {
-    background-color: #8d4288;
+	background-color: #8d4288;
 }
 
 .hobbies input {
-    width: 90%;
+	width: 90%;
 }
 
 .submit button {
-    border: 1px solid #521751;
-    color: #521751;
-    padding: 10px 20px;
-    font: inherit;
-    cursor: pointer;
+	border: 1px solid #521751;
+	color: #521751;
+	padding: 10px 20px;
+	font: inherit;
+	cursor: pointer;
 }
 
 .submit button:hover,
 .submit button:active {
-    background-color: #521751;
-    color: white;
+	background-color: #521751;
+	color: white;
 }
 
 .submit button[disabled],
 .submit button[disabled]:hover,
 .submit button[disabled]:active {
-    border: 1px solid #ccc;
-    background-color: transparent;
-    color: #ccc;
-    cursor: not-allowed;
+	border: 1px solid #ccc;
+	background-color: transparent;
+	color: #ccc;
+	cursor: not-allowed;
 }
 </style>
